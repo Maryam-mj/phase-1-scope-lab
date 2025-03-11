f273a644866fe653a40dcd34e3e91edc54c2fe05
Maryam-mj <mariamaliabdi12@gmail.com>
2025-03-11 12:55:57 -0700

    add assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase();
+     console.log(customerName)
+
+ }
+ 
+function setBestCustomer(){
+    bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+  
+const leastFavoriteCustomer = 'bus';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'may'
+    console.log(changeLeastFavoriteCustomer)
+}
+
